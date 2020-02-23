0894c683e0ca158fd638c96ea2e3099dcc468979
Muhayyo <muhayyo@hotmail.com>
2020-02-23 00:38:27 +0000

    set date on profile screen

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 function goBack() {
     window.history.go(-1);
 }
+
+// setting current date on profile screen
+var todaysDate = new Date().toLocaleString('en-us',{month:'long', year:'numeric', day:'numeric'});
+document.getElementById('currentDate').innerHTML = todaysDate;
